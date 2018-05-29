87740b1913f51f4722ee1f62a4a044c8fd625ee7
MeaSnaH <39258225+MeaSnaH@users.noreply.github.com>
2018-05-29 19:49:08 +0900

    update main
    split

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ package mindmapApplication;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 
 import javax.swing.*;
 
@@ -9,16 +10,29 @@ public class Main extends JFrame
 {
 	public Main()
 	{
+		setTitle("MindMapApp");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
 		JMenuBar mb = new JMenuBar();
-		JMenu screenMenu = new JMenu("File");
-
-		screenMenu.add(new JMenuItem("Save"));
-		screenMenu.add(new JMenuItem("Save As"));
-		screenMenu.add(new JMenuItem("Load"));
+		JMenu fileMenu = new JMenu("File");
 		
-		mb.add(screenMenu);
-		mb.add(new JMenu("Edit"));
+		fileMenu.add(new JMenuItem("Save"));
+		fileMenu.add(new JMenuItem("Save As"));
+		fileMenu.add(new JMenuItem("Load"));
 		
+		JMenu editMenu = new JMenu("Edit");
+		
+		JMenu toolsMenu = new JMenu("Tools");
+		
+		JMenu viewMenu = new JMenu("View");
+		
+		JMenu helpMenu = new JMenu("Help");
+		
+		mb.add(fileMenu);
+		mb.add(editMenu);
+		mb.add(toolsMenu);
+		mb.add(viewMenu);
+		mb.add(helpMenu);
 		setJMenuBar(mb);
 		
 		///////////////////
@@ -32,17 +46,23 @@ public class Main extends JFrame
 		getContentPane().add(toolBar, BorderLayout.NORTH);
 		
 		//////////////////
+		JTextField left = new JTextField();
+		left.setMinimumSize(new Dimension(250,600));
+		left.setPreferredSize(new Dimension(300,600));
+		JPanel middle = new JPanel();
+		middle.setMinimumSize(new Dimension(400,600));
+		middle.setPreferredSize(new Dimension(400,600));
+		JPanel right = new JPanel();
+		right.setMinimumSize(new Dimension(250,600));
+		right.setPreferredSize(new Dimension(300,600));
 		
-		JSplitPane split = new JSplitPane();
-		split.setOrientation(JSplitPane.HORIZONTAL_SPLIT);
-		split.setLeftComponent(new JButton("LEFT"));
-		split.setRightComponent(new JButton("Right"));
+		JSplitPane split = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,left,middle);
+		JSplitPane split2 = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,split,right);
 		
-		getContentPane().add(split);
+		getContentPane().add(split2);
 		
 		//////////////////
 		
-		setTitle("마인드맵");
 		setSize(1000, 600);
 		setVisible(true);
 	}
