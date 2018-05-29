01c139cd13fb1ded18e519281dab74f28e926245
enochjung <enoch-jung@naver.com>
2018-05-29 12:50:41 +0900

    Update Main.java
    dd

--- Main.java
+++ Main.java
@@ -1,9 +1,18 @@
 package mindmapApplication;
 
-public class Main
+import javax.swing.JFrame;
+
+public class Main extends JFrame
 {
+	public Main()
+	{
+		setTitle("마인드맵");
+		setSize(1000, 600);
+		setVisible(true);
+	}
+	
 	public static void main(String[] args)
 	{
-		System.out.println("Hello, world!");
+		Main frame = new Main();
 	}
 }
