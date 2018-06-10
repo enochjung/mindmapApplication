9f84ae4a9c3d92f291bd56156f149b7a8021b173
enochjung <enoch-jung@naver.com>
2018-06-10 20:16:19 +0900

    Update MindMapPanel
    선긋기 추가

--- JNode.java
+++ JNode.java
@@ -7,6 +7,7 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Insets;
 import java.awt.Point;
+import java.awt.Polygon;
 import java.awt.RenderingHints;
 import java.awt.Shape;
 import java.awt.event.MouseAdapter;
@@ -14,17 +15,17 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 import java.awt.geom.Area;
+import java.awt.geom.CubicCurve2D;
 import java.awt.geom.RoundRectangle2D;
-import java.util.ArrayList;
 import javax.swing.JLabel;
 import javax.swing.border.AbstractBorder;
 
 public class JNode extends JLabel
 {
-	private JNode parent;
-	private ArrayList<JNode> children;
+	private MindMapPanel mindMapPanel;
+	private JNode parentNode;
 	
-	public JNode(String label, int x, int y, int width, int height, Color color, JNode parent)
+	public JNode(MindMapPanel mindMapPanel, String label, int x, int y, int width, int height, Color color, JNode parentNode)
 	{
 		super(label);
 		setBounds(x, y, width, height);
@@ -35,13 +36,9 @@ public class JNode extends JLabel
 		MyMouseListener listener = new MyMouseListener();
 		addMouseListener(listener);
 		addMouseMotionListener(listener);
-		
-		this.parent = parent;
-	}
-	
-	public void addChild(JNode child)
-	{
-		children.add(child);
+
+		this.mindMapPanel = mindMapPanel;
+		this.parentNode = parentNode;
 	}
 	
 	private class MyMouseListener extends MouseAdapter implements MouseListener, MouseMotionListener
@@ -68,10 +65,10 @@ public class JNode extends JLabel
 			int y = start.y+e.getY()-mouse.y;
 			setLocation(x, y);
 			start.move(x, y);
+			mindMapPanel.repaint();
 		}
 	}
 	
-	// https://java-swing-tips.blogspot.com/2012/03/rounded-border-for-jtextfield.html
 	private class RoundedBorder extends AbstractBorder
 	{
 		private Color color;
@@ -95,7 +92,8 @@ public class JNode extends JLabel
 	        insets = new Insets(pad, pad, bottomPad, pad);
 		}
 
-		@Override public void paintBorder(Component c, Graphics g, int x, int y, int width, int height)
+		@Override
+		public void paintBorder(Component c, Graphics g, int x, int y, int width, int height)
 		{
 			Graphics2D g2 = (Graphics2D)g;
 			g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
@@ -128,9 +126,10 @@ public class JNode extends JLabel
 		}
 	}
 	
-	@Override protected void paintComponent(Graphics g)
+	@Override
+	public void paintComponent(Graphics g)
 	{
-		if (!isOpaque() && getBorder() instanceof RoundedBorder)
+		if(!isOpaque() && getBorder() instanceof RoundedBorder)
 		{
 			Graphics2D g2 = (Graphics2D) g.create();
 			g2.setPaint(getBackground());
@@ -139,9 +138,76 @@ public class JNode extends JLabel
 		}
 		super.paintComponent(g);
 	}
+
+	public Point getPoint(int position)
+	{
+		switch(position)
+		{
+		case 0: // above
+			return new Point(getX()+getWidth()/2, getY());
+		case 1: // right
+			return new Point(getX()+getWidth(), getY()+getHeight()/2);
+		case 2: // below
+			return new Point(getX()+getWidth()/2, getY()+getHeight());
+		case 3: // left
+			return new Point(getX(), getY()+getHeight()/2);
+		default:
+			throw new IllegalArgumentException("wrong position");
+		}
+	}
 	
+	private int distanceSq(Point p1, Point p2)
+	{
+		return (p1.x-p2.x)*(p1.x-p2.x) + (p1.y-p2.y)*(p1.y-p2.y);
+	}
+	
+	public void draw(Graphics2D g2)
+	{
+		if(parentNode == null)
+			throw new UnsupportedOperationException(this+" doesn't have parent node");
+		
+		int thisPosition = 0;
+		int parentPosition = 0;
+		int distance = distanceSq(this.getPoint(thisPosition), parentNode.getPoint(parentPosition));
+		for(int i=0; i<4; ++i)
+			for(int j=0; j<4; ++j)
+				if(distance > distanceSq(this.getPoint(i), parentNode.getPoint(j)))
+				{
+					distance = distanceSq(this.getPoint(i), parentNode.getPoint(j));
+					thisPosition = i;
+					parentPosition = j;
+				}
+
+		Point p1 = this.getPoint(thisPosition);
+		Point p2 = parentNode.getPoint(parentPosition);
+		
+		int length = (Math.abs(p1.x-p2.x) + Math.abs(p1.y-p2.y)) / 3;
+		int[] dx = {0, 1, 0, -1};
+		int[] dy = {-1, 0, 1, 0};
+
+		Point p1to = new Point(p1.x+dx[thisPosition]*length, p1.y+dy[thisPosition]*length);
+		Point p2from = new Point(p2.x+dx[parentPosition]*length, p2.y+dy[parentPosition]*length);
+
+		g2.setStroke(new BasicStroke(1));
+		g2.draw(new CubicCurve2D.Float(p1.x, p1.y, p1to.x, p1to.y, p2from.x, p2from.y, p2.x, p2.y));
+
+		Polygon p = new Polygon();
+		p.addPoint(p2.x, p2.y);
+		p.addPoint(p2.x+dx[parentPosition]*5+dx[(parentPosition+1)%4]*2, p2.y+dy[parentPosition]*5+dy[(parentPosition+1)%4]*2);
+		p.addPoint(p2.x+dx[parentPosition]*5+dx[(parentPosition+3)%4]*2, p2.y+dy[parentPosition]*5+dy[(parentPosition+3)%4]*2);
+		
+		g2.setStroke(new BasicStroke(5));
+		g2.draw(p);
+	}
+	
+	public JNode getParentNode()
+	{
+		return parentNode;
+	}
+	
+	@Override
 	public String toString()
 	{
-		return "JNode[text="+getText()+",x="+getX()+",y="+getY()+",width="+getWidth()+",height="+getHeight()+",color="+getBackground()+",parent="+parent+"]";
+		return "JNode[text="+getText()+",x="+getX()+",y="+getY()+",width="+getWidth()+",height="+getHeight()+",color="+getBackground()+",parent="+parentNode+"]";
 	}
 }
--- MindMapPanel.java
+++ MindMapPanel.java
@@ -2,24 +2,34 @@ package mindmapApplication;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.util.ArrayList;
 
 import javax.swing.JPanel;
 
 public class MindMapPanel extends JPanel
 {
+	ArrayList<JNode> nodes;
+	
 	public MindMapPanel(int width, int height)
 	{
 		super();
 		setLayout(null);
 		setMinimumSize(new Dimension(width, height));
+		
+		nodes = new ArrayList<>();
 	}
 	
 	public void makeNodes(String query)
 	{
-		ArrayList<JNode> nodes = new ArrayList<>();
+		ArrayList<JNode> tmpNodes = new ArrayList<>();
 		ArrayList<Integer> indent = new ArrayList<>();
-
+		ArrayList<Integer> depth = new ArrayList<>();
+		ArrayList<Color> color = new ArrayList<>();
+		
+		nodes.clear();
 		removeAll();
 		updateUI();
 		
@@ -28,8 +38,9 @@ public class MindMapPanel extends JPanel
 		{			
 			if(name.trim().equals(""))
 			{
-				nodes.add(null);
+				tmpNodes.add(null);
 				indent.add(999);
+				depth.add(999);
 				continue;
 			}
 			
@@ -38,26 +49,60 @@ public class MindMapPanel extends JPanel
 			
 			while(cnt<name.length() && name.charAt(cnt)=='\t')
 				++cnt;
+			
+			boolean found = false;
 			for(int i=indent.size()-1; i>=0; --i)
 				if(indent.get(i) < cnt)
 				{
-					parent = nodes.get(i);
+					found = true;
+					parent = tmpNodes.get(i);
+					depth.add(depth.get(i)+1);
 					break;
 				}
+			if(!found)
+				depth.add(0);
 			
 			name = name.trim();
 			int x = getRandom(60, 300);
 			int y = getRandom(100,500);
 			int width = 70+name.length()*10;
 			int height = 40;
-			Color color = new Color(getRandom(150,255), getRandom(150,255), getRandom(150,255));
-			nodes.add(new JNode(name, x, y, width, height, color, parent));
+			if(color.size() <= depth.get(depth.size()-1))
+				color.add(new Color(getRandom(150,255), getRandom(150,255), getRandom(150,255)));
+			tmpNodes.add(new JNode(this, name, x, y, width, height, color.get(depth.get(depth.size()-1)), parent));
 			indent.add(cnt);
 		}
 
-		for(JNode node : nodes)
+		for(JNode node : tmpNodes)
 			if(node != null)
+			{
 				add(node);
+				nodes.add(node);
+			}
+		
+		repaint();
+	}
+	
+	@Override
+	public void paintComponent(Graphics g)
+	{
+		super.paintComponent(g);
+		Graphics2D g2 = (Graphics2D)g;
+		
+		g2.setColor(new Color(150, 150, 150));
+		
+		g2.setRenderingHint(RenderingHints.KEY_ALPHA_INTERPOLATION, RenderingHints.VALUE_ALPHA_INTERPOLATION_QUALITY);
+		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		g2.setRenderingHint(RenderingHints.KEY_COLOR_RENDERING, RenderingHints.VALUE_COLOR_RENDER_QUALITY);
+		g2.setRenderingHint(RenderingHints.KEY_DITHERING, RenderingHints.VALUE_DITHER_ENABLE);
+		g2.setRenderingHint(RenderingHints.KEY_FRACTIONALMETRICS, RenderingHints.VALUE_FRACTIONALMETRICS_ON);
+		g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+		g2.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+		g2.setRenderingHint(RenderingHints.KEY_STROKE_CONTROL, RenderingHints.VALUE_STROKE_PURE);
+
+		for(JNode node : nodes)
+			if(node.getParentNode() != null)
+				node.draw(g2);
 	}
 	
 	private int getRandom(int a, int b)
